655abaa03762f7db2549b6320d87de5756209345
Jubair <jubair.ahmed7946@gmail.com>
2024-01-07 01:30:55 +0600

    Add: dummy modal added

--- src/components/PopularFoodSlide/PopularFoodSlide.jsx
+++ src/components/PopularFoodSlide/PopularFoodSlide.jsx
@@ -21,7 +21,22 @@ const PopularFoodSlide = ({ foods }) => {
       <div className="flex items-center justify-between mb-6">
         <h3 className="text-xl font-bold">Popular</h3>
         <div className="flex items-center gap-2">
-            <button className="btn btn-sm text-orange-500 mr-5">Add More</button>
+          <button className="btn btn-sm text-orange-500 mr-5" onClick={()=>document.getElementById('my_modal_1').showModal()}>Add More</button>
+          {/* modal content */}
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
           <MdChevronLeft
             onClick={sliderLeft}
             size={35}
