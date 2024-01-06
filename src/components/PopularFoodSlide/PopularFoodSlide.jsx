2a752fb712902b02d23e90be119cd1a10f4041b9
Jubair <jubair.ahmed7946@gmail.com>
2024-01-06 22:49:06 +0600

    Add: tailwindCSS scrollbar hide added

--- src/components/PopularFoodSlide/PopularFoodSlide.jsx
+++ src/components/PopularFoodSlide/PopularFoodSlide.jsx
@@ -1,5 +1,6 @@
 import Loader from "../Loader/Loader";
 
+
 /* eslint-disable react/prop-types */
 const PopularFoodSlide = ({foods}) => {
     
@@ -11,7 +12,7 @@ const PopularFoodSlide = ({foods}) => {
              <h3 className="text-xl font-bold">Popular</h3>
 
              {/* popular food row */}
-             <div className="w-full h-full whitespace-nowrap overflow-x-scroll scroll-smooth scrollbar-hide relative">
+             <div className="w-full h-full whitespace-nowrap overflow-x-scroll relative space-x-4 scrollbar-hide scroll-smooth">
                 {
                     foods?.map(food=> <img key={food.Id} src={food?.ImageUrl} className="w-52 h-64 object-cover rounded-xl cursor-pointer relative inline-block"/>)
                 }
