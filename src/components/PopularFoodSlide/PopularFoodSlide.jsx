38713f943246944c7e3e0b034d5174fca51e389d
Jubair <jubair.ahmed7946@gmail.com>
2024-01-07 01:15:37 +0600

    Add: slider created with js

--- src/components/PopularFoodSlide/PopularFoodSlide.jsx
+++ src/components/PopularFoodSlide/PopularFoodSlide.jsx
@@ -1,22 +1,40 @@
 import Loader from "../Loader/Loader";
-
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 /* eslint-disable react/prop-types */
-const PopularFoodSlide = ({foods}) => {
-    
-    if(foods.length<0){
-        return <Loader/>
-    }
-    return(
-        <div>
-             <h3 className="text-xl font-bold">Popular</h3>
+const PopularFoodSlide = ({ foods }) => {
+  const sliderLeft = () => {
+    let slider = document.getElementById("populer");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const sliderRight = () => {
+    let slider = document.getElementById("populer");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
-             {/* popular food row */}
-             <div className="w-full h-full whitespace-nowrap overflow-x-scroll relative space-x-4 scrollbar-hide scroll-smooth">
-                {
-                    foods?.map(food=> <img key={food.Id} src={food?.ImageUrl} className="w-52 h-64 object-cover rounded-xl cursor-pointer relative inline-block"/>)
-                }
-             </div>
-        </div>
-    )}
+  if (foods.length < 0) {
+    return <Loader />;
+  }
+  return (
+    <div>
+      <h3 className="text-xl font-bold">Popular</h3>
+
+      {/* popular food row */}
+      <MdChevronLeft onClick={sliderLeft} size={40} />
+      <MdChevronRight onClick={sliderRight} size={40}/>
+      <div
+        id="populer"
+        className="w-full h-full whitespace-nowrap overflow-x-scroll relative space-x-4 scrollbar-hide scroll-smooth"
+      >
+        {foods?.map((food) => (
+          <img
+            key={food.Id}
+            src={food?.ImageUrl}
+            className="w-52 h-64 object-cover rounded-xl cursor-pointer relative inline-block"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default PopularFoodSlide;
