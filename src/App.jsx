7bc77a7ac7d489e427932f1da96bda63e47a4bef
Jubair <jubair.ahmed7946@gmail.com>
2024-01-07 04:03:09 +0600

    Add: pre loader deleted

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,48 @@ import Navbar from "./components/Navbar/Navbar";
 import FoodSlide from "./components/FoodSlide/FoodSlide";
 import Loader from "./components/Loader/Loader";
 import { Toaster } from "react-hot-toast";
-import PreLoader from "./components/PreLoader/PreLoader";
 
 function App() {
-  const [foods, setFoods] = useState([])
+  const [foods, setFoods] = useState([]);
 
-  useEffect(()=>{
-    fetch("http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10")
-    .then(res=> res.json())
-    .then(data=> setFoods(data.Items))
-  },[])
+  useEffect(() => {
+    fetch(
+      "http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10"
+    )
+      .then((res) => res.json())
+      .then((data) => setFoods(data.Items));
+  }, []);
 
-  const recommendedItems = foods.filter(food=> food?.IsRecommended === true)
-  const popularItems = foods.filter(food=> food?.IsPopular === true)
+  const recommendedItems = foods.filter((food) => food?.IsRecommended === true);
+  const popularItems = foods.filter((food) => food?.IsPopular === true);
 
- 
-if(foods.length<0){
-  return <Loader/>
-}
+  if (foods.length < 0) {
+    return <Loader />;
+  }
   return (
     <>
-      <PreLoader/>
       <div className="max-w-screen-xl mx-auto px-4">
         {/* Navbar */}
-        <Navbar/>
+        <Navbar />
 
         {/* banner */}
-        <Banner/>
+        <Banner />
 
         {/* Popular food slide */}
-        <FoodSlide title="Popular" foods={popularItems} setFoods={setFoods} sliderId="1"/>
+        <FoodSlide
+          title="Popular"
+          foods={popularItems}
+          setFoods={setFoods}
+          sliderId="1"
+        />
         {/* Popular food slide */}
-        <FoodSlide title="Recommended" foods={recommendedItems} setFoods={setFoods} sliderId="2"/>
-        <Toaster/>
+        <FoodSlide
+          title="Recommended"
+          foods={recommendedItems}
+          setFoods={setFoods}
+          sliderId="2"
+        />
+        <Toaster />
       </div>
     </>
   );
