7e2d866acbf1bef4c3d9957e5a7d44730f4d288d
Jubair <jubair.ahmed7946@gmail.com>
2024-01-06 19:47:49 +0600

    Add: dummy navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,32 @@
+// icons
+import { CiSearch } from "react-icons/ci";
+
 const Navbar = () => {
-    return(
-        <div>
-             <p> HELLO I Am Navbar </p>
+  return (
+    <div className="max-w-screen-lg mx-auto">
+        {/* main container */}
+      <div className="flex items-center justify-between my-8 bg-base-100">
+        {/* logo */}
+        <div className="">
+          <a className="btn btn-ghost text-xl">foodio</a>
         </div>
-    )}
+        {/* search box */}
+        <div className="">
+          <div className="form-control">
+          <CiSearch/>
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto"
+            />
+          </div>
+          
+        </div>
+
+        {/* avatar */}
+        <img className="w-14" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_kSSoomJ9hiFXmiF2RdZlwx72Y23XsT6iwQ&usqp=CAU" alt="logo" />
+      </div>
+    </div>
+  );
+};
 export default Navbar;
