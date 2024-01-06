b0f44eab0bb31de8e5044b6b176634fd873c3e24
Jubair <jubair.ahmed7946@gmail.com>
2024-01-06 20:14:54 +0600

    Add: navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,73 @@
+// images
+import avatar2 from "../../assets/avatar-default.svg"
 // icons
-import { CiSearch } from "react-icons/ci";
+import { IoSearchOutline } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   return (
     <div className="max-w-screen-lg mx-auto">
-        {/* main container */}
-      <div className="flex items-center justify-between my-8 bg-base-100">
+      {/* main container */}
+      <div className="flex items-center justify-between py-8 ">
         {/* logo */}
-        <div className="">
-          <a className="btn btn-ghost text-xl">foodio</a>
-        </div>
+
+        <h2 className="text-2xl font-bold cursor-pointer">
+          foo<span className="text-orange-600">d</span>io
+        </h2>
+
         {/* search box */}
-        <div className="">
-          <div className="form-control">
-          <CiSearch/>
+        <div className="flex items-center gap-2">
+          <div className="form-control relative">
+            <IoSearchOutline
+              className="absolute bottom-3 left-2 text-orange-600"
+              size={20}
+            />
             <input
               type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
+              placeholder="Search food..."
+              className="input input-bordered lg:w-[500px] pl-8"
             />
           </div>
-          
+
+          {/* dropdown menu */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn m-1 uppercase">
+             Menu
+             <IoIosArrowDown size={20} className="text-orange-500 ml-4"/>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li className="font-semibold hover:text-orange-500">
+                <a>Home</a>
+              </li>
+              <li className="font-semibold hover:text-orange-500">
+                <a>Details</a>
+              </li>
+              <li className="font-semibold hover:text-orange-500">
+                <a>Category</a>
+              </li>
+              <li className="font-semibold hover:text-orange-500">
+                <a>My Favourites</a>
+              </li>
+              <li className="font-semibold hover:text-orange-500">
+                <a>Profile</a>
+              </li>
+              <li className="font-semibold hover:text-orange-500">
+                <a>Log In/Sign Up</a>
+              </li>
+            </ul>
+          </div>
         </div>
 
         {/* avatar */}
-        <img className="w-14" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_kSSoomJ9hiFXmiF2RdZlwx72Y23XsT6iwQ&usqp=CAU" alt="logo" />
+        <img
+
+          className="w-14"
+          src={avatar2}
+          alt="logo"
+        />
       </div>
     </div>
   );
