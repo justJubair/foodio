4d251a74044263b2c362eeccf8fe986e3ed3e0b9
Jubair <jubair.ahmed7946@gmail.com>
2024-01-07 16:49:42 +0600

    Add: footer is responsive for mobile as well

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import banner from "../../assets/Image1.png"
 const Banner = () => {
     return(
         // main container
-        <div className="flex flex-col items-center justify-center  bg-gradient-to-b from-orange-400 to-yellow-500 rounded-3xl my-6 pt-4 px-4 lg:px-0 md:flex-row mb-16 lg:mb-24">
+        <div className="flex flex-col items-center justify-center  bg-gradient-to-b from-orange-400 to-yellow-500 rounded-3xl my-6 pt-4 px-4 lg:px-0 md:flex-row mb-20 lg:mb-24">
              {/* text content */}
              <div className="w-full lg:w-1/3 text-white space-y-3 md:text-start">
                 <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium lg:leading-tight">Deliver Food To Your Door Step</h1>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import { FaGoogle } from "react-icons/fa";
 const Footer = () => {
   return (
     // main container
-    <div className="flex items-start justify-evenly bg-gradient-to-b from-orange-400 to-yellow-500 mt-32 md:mt-40 px-4 lg:px-0">
+    <div className="flex items-start justify-evenly bg-gradient-to-b from-orange-400 to-yellow-500 mt-28 md:mt-40 px-4 py-4 md:py-0 lg:px-0">
       {/* Search input and social logos */}
       <div className="space-y-16 mt-10">
         <div className="relative">
@@ -16,33 +16,39 @@ const Footer = () => {
             placeholder="Enter Your Email.."
             className="input input-bordered rounded-2xl w-full lg:w-[450px]"
           />
-          <button className="btn btn-sm bg-orange-600 text-white absolute md:right-3 lg:right-16 top-2 hover:bg-orange-500">
+          <button className="btn btn-sm bg-orange-600 text-white absolute right-2 md:right-3 lg:right-16 top-2 hover:bg-orange-500">
             Subscribe
             <FaArrowRightLong />
           </button>
         </div>
 
         {/* logo and social icons */}
-        <div className="flex items-end justify-between gap-20">
-         <div>
-         <h2 className="text-2xl font-bold cursor-pointer">
-            foo<span className="text-orange-700">d</span>io
-          </h2>
+        <div className="flex flex-col-reverse items-center justify-between gap-10 md:gap-20 md:flex-row md:items-end">
+          <div className="text-center md:text-start mb-4 md:mb-0">
+            <h2 className="text-2xl font-bold cursor-pointer mb-2 md:mb-0">
+              foo<span className="text-orange-700">d</span>io
+            </h2>
 
-          <p className="font-medium">
-            Copyright &copy; foodio. All Rights Reserved
-          </p>
-         </div>
+            <p className="font-medium">
+              Copyright &copy; foodio. All Rights Reserved
+            </p>
+          </div>
 
-         {/* icons */}
-         <div className="flex items-center gap-4">
-           
-        
-            <FaGoogle size={35} className="p-2 rounded-full bg-orange-100 text-orange-600"/>
-            <FaTwitter size={35} className="p-2 rounded-full bg-orange-100 text-orange-600"/>
-            <FaInstagram size={35} className="p-2 rounded-full bg-orange-100 text-orange-600"/>
-           
-         </div>
+          {/* icons */}
+          <div className="flex items-center gap-4">
+            <FaGoogle
+              size={35}
+              className="p-2 rounded-full text-white bg-orange-600 md:bg-orange-100 md:text-orange-600"
+            />
+            <FaTwitter
+              size={35}
+              className="p-2 rounded-full text-white bg-orange-600 md:bg-orange-100 md:text-orange-600"
+            />
+            <FaInstagram
+              size={35}
+              className="p-2 rounded-full text-white bg-orange-600 md:bg-orange-100 md:text-orange-600"
+            />
+          </div>
         </div>
       </div>
 
